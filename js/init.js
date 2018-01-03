5a92d882505e07300227d799ebf05f63642da60f
Isaac Boates <iboates@gmail.com>
2018-01-03 16:34:07 +0100

    can now change both pyramids with pyramid buttons

--- js/init.js
+++ js/init.js
@@ -56,39 +56,48 @@ $(document).ready(function() {
 
             layer.on('click', function (e) {
 
-                leftData[0].pine_vol = e.target.feature.properties._yr1999;
-                leftData[1].pine_vol = e.target.feature.properties._yr2000;
-                leftData[2].pine_vol = e.target.feature.properties._yr2001;
-                leftData[3].pine_vol = e.target.feature.properties._yr2002;
-                leftData[4].pine_vol = e.target.feature.properties._yr2003;
-                leftData[5].pine_vol = e.target.feature.properties._yr2004;
-                leftData[6].pine_vol = e.target.feature.properties._yr2005;
-                leftData[7].pine_vol = e.target.feature.properties._yr2006;
-                leftData[8].pine_vol = e.target.feature.properties._yr2007;
-                leftData[9].pine_vol = e.target.feature.properties._yr2008;
-                leftData[10].pine_vol = e.target.feature.properties._yr2009;
-                leftData[11].pine_vol = e.target.feature.properties._yr2010;
-                leftData[12].pine_vol = e.target.feature.properties._yr2011;
-                leftData[13].pine_vol = e.target.feature.properties._yr2012;
-                leftData[14].pine_vol = e.target.feature.properties._yr2013;
-                leftData[15].pine_vol = e.target.feature.properties._yr2014;
-                
-                rightData[0].pine_vol = e.target.feature.properties._yr1999;
-                rightData[1].pine_vol = e.target.feature.properties._yr2000;
-                rightData[2].pine_vol = e.target.feature.properties._yr2001;
-                rightData[3].pine_vol = e.target.feature.properties._yr2002;
-                rightData[4].pine_vol = e.target.feature.properties._yr2003;
-                rightData[5].pine_vol = e.target.feature.properties._yr2004;
-                rightData[6].pine_vol = e.target.feature.properties._yr2005;
-                rightData[7].pine_vol = e.target.feature.properties._yr2006;
-                rightData[8].pine_vol = e.target.feature.properties._yr2007;
-                rightData[9].pine_vol = e.target.feature.properties._yr2008;
-                rightData[10].pine_vol = e.target.feature.properties._yr2009;
-                rightData[11].pine_vol = e.target.feature.properties._yr2010;
-                rightData[12].pine_vol = e.target.feature.properties._yr2011;
-                rightData[13].pine_vol = e.target.feature.properties._yr2012;
-                rightData[14].pine_vol = e.target.feature.properties._yr2013;
-                rightData[15].pine_vol = e.target.feature.properties._yr2014;
+                var pyramidActionCode = $("input[name=pyramid]:checked").val();
+
+                if (pyramidActionCode === "replace-left-pyramid") {
+
+                    leftData[0].pine_vol = e.target.feature.properties._yr1999;
+                    leftData[1].pine_vol = e.target.feature.properties._yr2000;
+                    leftData[2].pine_vol = e.target.feature.properties._yr2001;
+                    leftData[3].pine_vol = e.target.feature.properties._yr2002;
+                    leftData[4].pine_vol = e.target.feature.properties._yr2003;
+                    leftData[5].pine_vol = e.target.feature.properties._yr2004;
+                    leftData[6].pine_vol = e.target.feature.properties._yr2005;
+                    leftData[7].pine_vol = e.target.feature.properties._yr2006;
+                    leftData[8].pine_vol = e.target.feature.properties._yr2007;
+                    leftData[9].pine_vol = e.target.feature.properties._yr2008;
+                    leftData[10].pine_vol = e.target.feature.properties._yr2009;
+                    leftData[11].pine_vol = e.target.feature.properties._yr2010;
+                    leftData[12].pine_vol = e.target.feature.properties._yr2011;
+                    leftData[13].pine_vol = e.target.feature.properties._yr2012;
+                    leftData[14].pine_vol = e.target.feature.properties._yr2013;
+                    leftData[15].pine_vol = e.target.feature.properties._yr2014;
+
+                } else if (pyramidActionCode === "replace-right-pyramid") {
+
+                    rightData[0].pine_vol = e.target.feature.properties._yr1999;
+                    rightData[1].pine_vol = e.target.feature.properties._yr2000;
+                    rightData[2].pine_vol = e.target.feature.properties._yr2001;
+                    rightData[3].pine_vol = e.target.feature.properties._yr2002;
+                    rightData[4].pine_vol = e.target.feature.properties._yr2003;
+                    rightData[5].pine_vol = e.target.feature.properties._yr2004;
+                    rightData[6].pine_vol = e.target.feature.properties._yr2005;
+                    rightData[7].pine_vol = e.target.feature.properties._yr2006;
+                    rightData[8].pine_vol = e.target.feature.properties._yr2007;
+                    rightData[9].pine_vol = e.target.feature.properties._yr2008;
+                    rightData[10].pine_vol = e.target.feature.properties._yr2009;
+                    rightData[11].pine_vol = e.target.feature.properties._yr2010;
+                    rightData[12].pine_vol = e.target.feature.properties._yr2011;
+                    rightData[13].pine_vol = e.target.feature.properties._yr2012;
+                    rightData[14].pine_vol = e.target.feature.properties._yr2013;
+                    rightData[15].pine_vol = e.target.feature.properties._yr2014;
+
+                }
+
                 update(leftData, rightData);
 
             });
@@ -239,7 +248,8 @@ $(document).ready(function() {
     var leftXAxis = leftSvg.append("g")
         .attr("transform", "translate(0," + leftHeight + ")")
         .call(d3.axisBottom(leftXScale)
-            .tickFormat(d3.formatPrefix(".2", 1e6)));
+            .tickFormat(d3.formatPrefix(".2", 1e6))
+            .ticks(5));
 
     // add the y Axis
     var leftYAxis = leftSvg.append("g")
@@ -285,26 +295,25 @@ $(document).ready(function() {
     var rightXAxis = rightSvg.append("g")
         .attr("transform", "translate(0," + rightHeight + ")")
         .call(d3.axisBottom(rightXScale)
-            .tickFormat(d3.formatPrefix(".2", 1e6)));
+            .tickFormat(d3.formatPrefix(".2", 1e6))
+            .ticks(5));
 
     // add the y Axis
     var rightYAxis = rightSvg.append("g")
         .attr("transform", "translate( " + 0 + ", 0 )");
 
-
-
-
+    // updating pyramids
 
     function update(newLeftData, newRightData) {
 
-        //console.log("attempting update");
+        // get the maximum of both sets of data to standardize both x-axes
+        var absoluteMax = Math.max(d3.max(newLeftData, function(d){ return d.pine_vol; }),
+                              d3.max(newRightData, function(d){ return (d.pine_vol); }));
 
-        leftXScale.domain([0, d3.max(newLeftData, function(d){ return d.pine_vol; })]);
+        leftXScale.domain([0, absoluteMax]);
         leftYScale.domain(newLeftData.map(function(d) { return d.year; }));
-        //leftXScale.domain([0, d3.max(newLeftData, function(d){ return (d.pine_vol); })]);
-        //leftYScale.domain(newLeftData.map(function(d) { return d.year; }));
 
-        rightXScale.domain([0, d3.max(newRightData, function(d){ return (d.pine_vol); })]);
+        rightXScale.domain([0, absoluteMax]);
         rightYScale.domain(newRightData.map(function(d) { return d.year; }));
 
         //console.log("domains defined, attempting to remove bars");
@@ -342,13 +351,15 @@ $(document).ready(function() {
         leftXAxis = leftSvg.append("g")
             .attr("transform", "translate(0," + leftHeight + ")")
             .call(d3.axisBottom(leftXScale)
-                .tickFormat(d3.formatPrefix(".2", 1e6)));
+                .tickFormat(d3.formatPrefix(".2", 1e6))
+                .ticks(5));
         
         rightXAxis.remove();
         rightXAxis = rightSvg.append("g")
             .attr("transform", "translate(0," + rightHeight + ")")
             .call(d3.axisBottom(rightXScale)
-                .tickFormat(d3.formatPrefix(".2", 1e6)));
+                .tickFormat(d3.formatPrefix(".2", 1e6))
+                .ticks(5));
 
     }
 
